106a280036cb1f055fa335be84f77785df7c3724
= <bottenbergdave@gmail.com>
2024-08-29 07:58:25 -0400

    updated messages array

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,22 +1,41 @@
 const messages = [
-    {
-        text: "Hi there!",
-        user: "David B.",
-        added: new Date()
-    },
-    {
-        text: "Hello World",
-        user: "Tech Guy",
-        added: new Date()
-    }
+  {
+    text: "Hi there!",
+    user: "David B.",
+    added: new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    }) + ' ' + new Date().toLocaleTimeString(),
+  },
+  {
+    text: "Hello World",
+    user: "Tech Guy",
+    added: new Date().toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    }) + ' ' + new Date().toLocaleTimeString(),
+  },
 ];
 
 module.exports = {
-    get: (req,res) => {
-        res.render('index', {title: "Mini Messageboard", messages:messages})
-    },
-    post: (req,res) => {
-        messages.push({text:req.body.userMessage, user:req.body.userName, added: new Date()});
-        res.redirect('/')
-    },
-}
+  get: (req, res) => {
+    res.render("index", { title: "Mini Message Board", messages: messages });
+  },
+  post: (req, res) => {
+    messages.push({
+      text: req.body.userMessage,
+      user: req.body.userName,
+      added: new Date().toLocaleDateString("en-us", {
+        weekday: "long",
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })+ ' ' + new Date().toLocaleTimeString(),
+    });
+    res.redirect("/");
+  },
+};
