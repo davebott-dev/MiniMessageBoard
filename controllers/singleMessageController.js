5b6b03def4c51b4ee4aae1aaf7bca874492830fe
= <bottenbergdave@gmail.com>
2024-08-31 07:33:24 -0400

    displays specified message

--- controllers/singleMessageController.js
+++ controllers/singleMessageController.js
@@ -2,12 +2,12 @@ const messages = require("./messageArr");
 
 module.exports = {
   get: (req, res) => {
-    const { messageId } = req.params;
+    const  {messageId}  = req.params;
     res.render("singleMessage", {
       text: messages[messageId - 1].text,
       user: messages[messageId - 1].user,
       added: messages[messageId - 1].added,
     });
-    console.log(messages[messageId - 1]);
+    
   },
 };
