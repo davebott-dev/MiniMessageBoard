281596a296fd78b96124979d8bc3807547da7dc5
= <bottenbergdave@gmail.com>
2024-09-07 12:26:39 -0400

    update routes to handle specific controller function

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express=  require('express');
 const router = express.Router();
 const controller = require('../controllers/indexController');
 
+
 router.get('/',controller.importData);
 router.post("/new", controller.createDataPost);
+router.post('/message/:messageId',controller.deletePost)
 
 module.exports = router;
--- routes/singleMessage.js
+++ routes/singleMessage.js
@@ -2,6 +2,6 @@ const express= require('express');
 const router = express.Router();
 const controller = require('../controllers/singleMessageController');
 
-router.get('/:messageId',controller.get);
+router.get('/:messageId',controller.getSinglePost);
 
 module.exports = router;
