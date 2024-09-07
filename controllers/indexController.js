3bb9ba450c70e93f657e9e3f1f2756c2e3f6e411
= <bottenbergdave@gmail.com>
2024-09-07 12:23:18 -0400

    added form validations and error handling

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,27 +1,52 @@
 const db = require("../models/query");
+const { body, validationResult } = require("express-validator");
 
-async function importData(req, res) {
+const validations = [
+  body("text")
+    .trim()
+    .isLength({ min: 1, max: 50 })
+    .withMessage(
+      "Your message could not be added to the message board. Your message must be between 1 and 50 characters!"
+    ),
+  body("username")
+    .trim()
+    .isLength({ min: 1, max: 15 })
+    .withMessage(
+      "Your username could not be added to the message board. Your username must be between 1 and 15 characters!"
+    ),
+];
+
+exports.importData = async (req, res) => {
   const data = await db.getData();
-  console.log(data);
   res.render("index", { title: "Mini Message Board", messages: data });
-}
+};
 
-async function createDataPost(req, res) {
-  const formData = req.body;
-  await db.insertData(
-    formData.text,
-    formData.username,
-    new Date().toLocaleDateString("en-us", {
-      weekday: "long",
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-    })+ ' '+ new Date().toLocaleTimeString()
-  );
+exports.createDataPost = [
+  validations,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("error", { errors: errors.array() });
+    }
+    const formData = req.body;
+    await db.insertData(
+      formData.text,
+      formData.username,
+      new Date().toLocaleDateString("en-us", {
+        weekday: "long",
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      }) +
+        " " +
+        new Date().toLocaleTimeString()
+    );
+    res.redirect("/");
+  },
+];
+
+exports.deletePost = async (req, res) => {
+  const index = req.params.messageId;
+  const data = await db.deleteData(index);
   res.redirect("/");
-}
-
-module.exports = {
-  importData,
-  createDataPost,
 };
