14186d861820febf536a4c6d43334fbeb107d110
= <bottenbergdave@gmail.com>
2025-02-05 08:32:24 -0500

    updates

--- models/pool.js
+++ models/pool.js
@@ -1,10 +1,10 @@
 const {Pool} = require('pg');
+require("dotenv").config();
 
 module.exports = new Pool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    PORT: process.env.PORT
-})
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
